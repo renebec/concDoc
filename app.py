c4a12d6d69775af400ce1213d8cd27a27d9380b1
renebec <39104197+renebec@users.noreply.github.com>
2025-11-18 21:18:05 +0000

    app run debug last line app.py

--- app.py
+++ app.py
@@ -578,12 +578,14 @@ def logout():
     return redirect(url_for('login'))
 
 
-"""
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-"""
 
+
+"""
 if __name__ == '__main__':
     http_server = WSGIServer(('0.0.0.0', 8080), app)
     http_server.serve_forever()
+"""
